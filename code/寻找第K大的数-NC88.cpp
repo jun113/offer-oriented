4c745b461f9514ca2503d2cc282e08e3889d45a1
Junlin‘W <by113123@163.com>
2021-08-18 17:46:08 +0800

    fix bug NC119, update NC88

--- code/寻找第K大的数-NC88.cpp
+++ code/寻找第K大的数-NC88.cpp
@@ -1,30 +1,29 @@
 class Solution {
  public:
-  vector<int> GetLeastNumbers_Solution(vector<int> input, int k) {
-    if (k == 0) return vector<int> ();
-    if (k >= input.size()) return input;
-
-    // 基于快排的最小K个数
-    QuickSort(input, 0, input.size()-1, k);
-    return vector<int>({input.begin(), input.begin() + k});
+  int findKth(vector<int> a, int n, int K) {
+    QuickSort(a, 0, n-1, K);
+    return a[K-1];
   }
  private:
+//--------------------------------------------------------
+  // 基于快排，找出第K大数，包括重复元素 不用去重
   void QuickSort(vector<int> &arr, const int low, const int high, const int k) {
     if (low > high) return ;
     int privot = Partition(arr, low, high);
-    if (privot == k-1) return;
+    if (privot == k-1) return ;
     else if (privot >= k) QuickSort(arr, low, privot-1, k);
     else QuickSort(arr, privot+1, high, k);
   }
   int Partition(vector<int> &arr, int low, int high) {
     int temp = arr[low];
     while (low < high) {
-      while (low < high && temp <= arr[high]) --high;
+      while (low < high && temp >= arr[high]) --high;
       arr[low] = arr[high];
-      while (low < high && arr[low] <= temp) ++low;
+      while (low < high && arr[low] >= temp) ++low;
       arr[high] = arr[low];
     }
     arr[low] = temp;
     return low;
   }
+//-----------------------------------------------------
 };
