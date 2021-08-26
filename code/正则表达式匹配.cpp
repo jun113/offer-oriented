4ac8119afb5d14776faf6af822950788b6c16e79
Junlin‘W <by113123@163.com>
2021-08-26 15:12:16 +0800

    Create 正则表达式匹配.cpp

---
+++ code/正则表达式匹配.cpp
@@ -0,0 +1,26 @@
+/*
+  leetcode id: 10
+  '.': 匹配任意单个字符
+  '*': 匹配任意个前一个字符
+*/
+class Solution {
+ public:
+  bool isMatch(string s, string p) {
+    bool result;
+    myIsMatch(s, p, result);
+    return result;
+
+  }
+ private:
+  void myIsMatch(string s, string p, bool &ans) {
+    ans = match(s.data(), p.data());
+  }
+  bool match(char *s, char *p) {
+    if (!*p) return !*s;
+    if (*(p + 1) != '*') {
+      return *s == *p || (*p == '.' && *s != '\0') ? match(s + 1, p + 1) : false;
+    } else {
+      return *s == *p || (*p == '.' && *s != '\0') ? match(s, p + 2) || match(s + 1, p) : match(s, p + 2);
+    }
+  }
+};
